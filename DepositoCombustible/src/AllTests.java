2fb297eacf9bf10fd03ba7ad75a25b1bfcc82b89
Adrián Vicente Maciá <adrikhz@gmail.com>
2021-02-04 10:52:25 +0100

    Suite de pruebas

---
+++ DepositoCombustible/src/AllTests.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ConsumirTest.class, EstaLlenoTest.class, EstaVacioTest.class, FillTest.class, GetDepositoMaxTest.class,
+		GetDepositoNivelTest.class })
+public class AllTests {
+
+}
