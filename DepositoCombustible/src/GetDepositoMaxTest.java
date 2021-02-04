b7f1f81e3334a748d9b765ad20e3fc4857bdc55f
Adrián Vicente Maciá <adrikhz@gmail.com>
2021-02-04 10:31:25 +0100

    Test de getDepositoMax

--- DepositoCombustible/src/GetDepositoMaxTest.java
+++ DepositoCombustible/src/GetDepositoMaxTest.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class DepositoCombustibleTest {
+public class GetDepositoMaxTest {
 
 	@Test
 	public void testGetDepositoMax() {
